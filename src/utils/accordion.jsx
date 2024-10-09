3afb16d9c6830d95a84f7d7b54fa273e68d770e3
aswinV <137137358+aswin12122@users.noreply.github.com>
2024-10-09 15:47:57 +0530

    Add files via upload

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+     "We're dedicated to offering exceptional services that enhance your living experience. A great home can transform your life, and we provide the best options with competitive interest rates",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+    "We ensure stable pricing, offering you the best value without unexpected fluctuations. Our goal is to provide transparency and reliability in every transaction."
+, },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+     "We offer the most competitive prices on the market, ensuring you get the best value for your investment without compromise.",
+      },
+];
+export default data;
